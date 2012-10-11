e5b6d24b94122c01d92594b1975976238d4f90c6
Arild Nilsen <arild.nilsen2@gmail.com>
2012-10-11 21:58:02 +0200

    Added simple future examples

--- build.sbt
+++ build.sbt
@@ -2,7 +2,5 @@ name := "Actors Presentation"
 
 scalaVersion := "2.10.0-M7"
 
-resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
-
-libraryDependencies ++= Seq("com.typesafe.akka" % "akka-actor" % "2.0.3", "com.typesafe.akka" % "akka-remote" % "2.0.3", "com.typesafe.akka" % "akka-testkit" % "2.0.3", "org.specs2" % "specs2_2.10.0-M7" % "1.12.1.1", "junit" % "junit" % "4.5" % "test", "org.scala-lang" % "scala-actors" % "2.10.0-M7")
+libraryDependencies ++= Seq("org.specs2" % "specs2_2.10.0-M7" % "1.12.1.1", "junit" % "junit" % "4.5" % "test", "org.scala-lang" % "scala-actors" % "2.10.0-M7")
 
--- src/main/scala/actors/ComputeActor.scala
+++ src/main/scala/actors/ComputeActor.scala
@@ -1,7 +1,12 @@
 package actors
 
 import scala.actors.Actor
-import scala.actors.Futures._
+import scala.concurrent.Future
+import scala.concurrent.future
+import scala.concurrent.Await
+import scala.concurrent.util._
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.Random
 
 abstract class Work {
   def perform(): Any
@@ -14,45 +19,120 @@ class SumSequence(start: Int, stop: Int) extends Work {
   }
 }
 
-case object HeavyWork {
-  
-}
-
-//class HeavyWork(x: Int) extends Work {
-//  def perform(): Int = {
-//    Thread.sleep(10000)
-//    x
-//  }
-//}
-
 class ComputeActor extends Actor {
   def computeSquare(i: Int) = i * i
   def computeLength(s: String) = s.length()
 
   def act() {
-    receive {
-      case i: Int => sender ! computeSquare(i)
-      case s: String => sender ! computeLength(s);
-      case w: Work => sender ! w.perform()
-    }
-  }
-
-  def doSomeHeavyLifting() = future {
-    future {
-      //      Thread.sleep(5000)
-      println("omg, the future arrived")
-      42
+    while (true) {
+      receive {
+        case i: Int => sender ! computeSquare(i)
+        case s: String => sender ! computeLength(s);
+        case w: Work => sender ! w.perform()
+      }
     }
   }
 }
 
-object Asdf {
-  def main(args: Array[String]) {
+object Futures extends App {
+  def lightWork(): Future[Int] = {
     val f = future {
-      //      Thread.sleep(5000)
-      println("omg, the future arrived")
-      42
+      new SumSequence(0, 1).perform
     }
-    println(f())
+    f.mapTo[Int]
   }
+
+  def heavyWork(): Future[Int] = {
+    val f = future {
+      Thread.sleep(1000)
+      new SumSequence(0, 5).perform
+    }
+    f.mapTo[Int]
+  }
+
+  def riskyWork(): Future[Int] = {
+    val f = future {
+      val mightBeNegative = Random.nextInt(2) - 1
+      new SumSequence(mightBeNegative, 5).perform
+    }
+    f.mapTo[Int]
+  }
+
+  def helloWorld() = {
+    println("Test print before future")
+    val s = "Hello"
+    val f = future { s + " future!" }
+    f onSuccess { case v => println(v) }
+    println("Test print after future")
+    Await.ready(f, Duration.Inf) // Blocks until future is ready
+  }
+
+  def simpleTransformations() = {
+    val f = future {
+      Thread.sleep(1000)
+      1 + 1
+    }.map(_ + 1) // Completely asynchronously
+    f onSuccess { case v => println(v) }
+    Await.ready(f, Duration.Inf)
+  }
+
+  def safeTypecast() = {
+    val f: Future[Any] = future {
+      new SumSequence(0, 2000).perform()
+    }
+    val f2 = f
+    f2 onSuccess { case v => println(v) }
+    Await.ready(f2, Duration.Inf)
+  }
+
+  def selectFutureFinishedFirst() = {
+    val result = lightWork() either heavyWork() // Light work (should) always win
+    result onSuccess { case v => println(v) }
+    Await.ready(result, Duration.Inf)
+
+    // Alternative way
+    val sequenceOfFutures = Seq(lightWork(), heavyWork())
+    val result2 = Future.firstCompletedOf(sequenceOfFutures)
+    result2 onSuccess { case v => println(v) }
+    Await.ready(result2, Duration.Inf)
+  }
+
+  def sumOfMultipleResults() = {
+    val sequenceOfFutures = Seq(lightWork(), heavyWork())
+    val futureOfSequence: Future[Seq[Int]] = Future.sequence(sequenceOfFutures)
+    futureOfSequence onSuccess { case v => println("Sum: " + v.sum) }
+    Await.ready(futureOfSequence, Duration.Inf)
+  }
+
+  def sumOfMultipleResultsWithPowerTools() = {
+    val sequenceOfFutures: Seq[Future[Int]] = Seq(lightWork(), heavyWork())
+    val sum: Future[Int] = Future.reduce(sequenceOfFutures)(_ + _) // Asynchronous
+    sum onSuccess { case v => println(v) }
+    println("Waiting for reduce to complete")
+    Await.ready(sum, Duration.Inf)
+  }
+
+  def failureRecovery() = {
+    val sum = riskyWork() recoverWith { case e: IllegalArgumentException => lightWork() }
+    sum onSuccess { case v => println("Success: " + v) } // onFailure will not trigger with recoverWith set
+    Await.ready(sum, Duration.Inf)
+  }
+
+  def forComprehension() = {
+    val f = for {
+      v1 <- lightWork()
+      v2 <- heavyWork()
+    } yield v1 + v2 // Yields a new future
+
+    f onSuccess { case v => println(v) }
+    Await.ready(f, Duration.Inf)
+  }
+
+  //  helloWorld
+  //  simpleTransformations
+  //  selectFutureFinishedFirst
+  //  sumOfMultipleResultsWithPowerTools
+  //  forComprehension
+  failureRecovery
+
 }

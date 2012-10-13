92590d4b2138acef39608f9b412f7ceedc68c730
Arild Nilsen <arild.nilsen2@gmail.com>
2012-10-13 17:46:28 +0200

    Added more advanced future examples

--- src/main/scala/actors/ComputeActor.scala
+++ src/main/scala/actors/ComputeActor.scala
@@ -19,6 +19,31 @@ class SumSequence(start: Int, stop: Int) extends Work {
   }
 }
 
+class FactorNumber(n: Long) extends Work {
+  def perform(): List[Long] = {
+    faktor(n)
+  }
+
+  /**
+   * Faktoriserer et tall, på naivt vis
+   */
+  def faktor(n: Long): List[Long] = {
+
+    def faktor0(a: Long, acc: List[Long]): List[Long] = {
+      def isFaktor(f: Long, p: Long): Boolean = p % f == 0
+      if (a > 1) {
+        if (isFaktor(a, n))
+          faktor0(a - 1, List(a) ::: acc)
+        else
+          faktor0(a - 1, acc)
+      } else {
+        acc
+      }
+    }
+    faktor0(n - 1, List())
+  }
+}
+
 class ComputeActor extends Actor {
   def computeSquare(i: Int) = i * i
   def computeLength(s: String) = s.length()
@@ -71,7 +96,7 @@ object Futures extends App {
     val f = future {
       Thread.sleep(1000)
       1 + 1
-    }.map(_ + 1) // Completely asynchronously
+    }.map(x => x + 1) // Completely asynchronously
     f onSuccess { case v => println(v) }
     Await.ready(f, Duration.Inf)
   }
@@ -128,11 +153,63 @@ object Futures extends App {
     Await.ready(f, Duration.Inf)
   }
 
+  def factorNumbersInParallel() = {
+    // Wraps a number into a future that computes the number's factors
+    def toFutureFactors(number: Long): Future[Seq[Long]] = {
+      future {
+        println("Original future is executing");
+        val f = new FactorNumber(number)
+        val res = f.perform()
+        println("Original future is done");
+        res
+      }
+    }
+    val f1 = toFutureFactors(545515193L)
+    val f2 = toFutureFactors(958284559L)
+    val f3 = toFutureFactors(215487021L)
+
+    // Elegant way
+//    val res1: Seq[Future[Seq[Long]]] = Seq(f1, f2, f3)
+//    val res2 = Future.fold(res1)(0L)((r, c) => {
+//      Math.max(r, c.max)
+//    })
+//    res2 onSuccess { case v => println("Result: " + v) }
+//    println("Waiting for result...")
+//    Await.ready(res2, Duration.Inf)
+
+    // Not so elegant way
+    val res1: Seq[Future[Seq[Long]]] = Seq(f1, f2, f3)
+    val res2: Future[Seq[Seq[Long]]] = Future.sequence(res1)
+    val res3 = res2.map(factorSequences => {
+      println("All factors computed. Finding max now.")
+      factorSequences.foldLeft(0L)((r, c) => Math.max(r, c.max))
+    })
+    res3 onSuccess { case v => println("Result: " + v) }
+    println("Waiting for result...")
+    Await.ready(res3, Duration.Inf)
+
+    // Finer granularity on tasks: Finding max of sub-results are wrapped into new futures
+//    val factorSequences: Seq[Future[Seq[Long]]] = Seq(f1, f2, f3)
+//    val maxFactors: Seq[Future[Long]] = factorSequences.map(f => {
+//      println("Creating new future")
+//      f.map({
+//        factors =>
+//          Thread.sleep(1000 + Random.nextInt(3000))
+//          println("Sub-result done: " + factors)
+//          factors.max
+//      })
+//    })
+//    val res: Future[Seq[Long]] = Future.sequence(maxFactors)
+//    res onSuccess { case v => println("Final result: " + v.max) }
+//    println("Waiting for result...")
+//    Await.ready(res, Duration.Inf)
+  }
   //  helloWorld
   //  simpleTransformations
   //  selectFutureFinishedFirst
   //  sumOfMultipleResultsWithPowerTools
   //  forComprehension
-  failureRecovery
+  // failureRecovery
+  factorNumbersInParallel
 
 }
